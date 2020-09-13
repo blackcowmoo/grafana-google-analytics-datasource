5caa16d38cd288666eb4dce08e2ed498c6b83c1f
micalgenus <micalgenus@gmail.com>
2020-09-13 17:50:26 +0900

    Init go

---
+++ pkg/analytics.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+
+	"github.com/grafana/grafana-plugin-sdk-go/backend"
+	"github.com/grafana/grafana-plugin-sdk-go/backend/datasource"
+	"github.com/grafana/grafana-plugin-sdk-go/backend/instancemgmt"
+)
+
+// newDatasource returns datasource.ServeOpts.
+func newDatasource() datasource.ServeOpts {
+	// creates a instance manager for your plugin. The function passed
+	// into `NewInstanceManger` is called when the instance is created
+	// for the first time or when a datasource configuration changed.
+	im := datasource.NewInstanceManager(newDataSourceInstance)
+	ds := &SampleDatasource{
+		im: im,
+	}
+
+	return datasource.ServeOpts{
+		QueryDataHandler:   ds,
+		CheckHealthHandler: ds,
+	}
+}
+
+type AnalyticsDatasource struct {
+	// The instance manager can help with lifecycle management
+	// of datasource instances in plugins. It's not a requirements
+	// but a best practice that we recommend that you follow.
+	im instancemgmt.InstanceManager
+}
+
+// CheckHealth handles health checks sent from Grafana to the plugin.
+// The main use case for these health checks is the test button on the
+// datasource configuration page which allows users to verify that
+// a datasource is working as expected.
+func (td *SampleDatasource) CheckHealth(ctx context.Context, req *backend.CheckHealthRequest) (*backend.CheckHealthResult, error) {
+	var status = backend.HealthStatusOk
+
+	return &backend.CheckHealthResult{
+		Status:  status,
+		Message: message,
+	}, nil
+}
