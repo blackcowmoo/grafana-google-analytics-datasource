a752bc1faf87e982e354ec4668c6f0582ad9f947
GyeongSu Han <micalgenus@gmail.com>
2020-09-19 02:11:40 +0900

    feature: Use secure json data in backend datasource  (#2)
    * Init lint
    
    * Revert "Init lint"
    
    This reverts commit d47627015d8e9e125f72f958a63593ff88298ce8.
    
    * Init go
    
    * Initialize AnalyticsDatasource
    
    * Fix lint

--- go.mod
+++ go.mod
@@ -2,4 +2,15 @@ module github.com/grafana/simple-datasource-backend
 
 go 1.14
 
-require github.com/grafana/grafana-plugin-sdk-go v0.77.0
+require (
+	github.com/go-macaron/session v0.0.0-20200329073812-7d919ce6a8d2 // indirect
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	github.com/gosimple/slug v1.9.0 // indirect
+	github.com/grafana/grafana v6.1.6+incompatible
+	github.com/grafana/grafana-plugin-sdk-go v0.77.0
+	github.com/inconshreveable/log15 v0.0.0-20200109203555-b30bc20e4fd1 // indirect
+	github.com/mattn/go-colorable v0.1.7 // indirect
+	github.com/teris-io/shortid v0.0.0-20171029131806-771a37caa5cf // indirect
+	gopkg.in/ini.v1 v1.61.0 // indirect
+	gopkg.in/macaron.v1 v1.3.9 // indirect
+)
---
+++ pkg/analytics.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/grafana/grafana-plugin-sdk-go/backend"
+	"github.com/grafana/grafana-plugin-sdk-go/backend/datasource"
+	"github.com/grafana/grafana-plugin-sdk-go/backend/instancemgmt"
+)
+
+// newDatasource returns datasource.ServeOpts.
+func newDatasource() datasource.ServeOpts {
+	// creates a instance manager for your plugin. The function passed
+	// into `NewInstanceManger` is called when the instance is created
+	// for the first time or when a datasource configuration changed.
+	im := datasource.NewInstanceManager(newDataSourceInstance)
+	ds := &AnalyticsDatasource{
+		im: im,
+	}
+
+	return datasource.ServeOpts{
+		// QueryDataHandler:   ds,
+		CheckHealthHandler: ds,
+	}
+}
+
+type AnalyticsDatasource struct {
+	// The instance manager can help with lifecycle management
+	// of datasource instances in plugins. It's not a requirements
+	// but a best practice that we recommend that you follow.
+	im instancemgmt.InstanceManager
+}
+
+// CheckHealth handles health checks sent from Grafana to the plugin.
+// The main use case for these health checks is the test button on the
+// datasource configuration page which allows users to verify that
+// a datasource is working as expected.
+func (td *AnalyticsDatasource) CheckHealth(ctx context.Context, req *backend.CheckHealthRequest) (*backend.CheckHealthResult, error) {
+	var status = backend.HealthStatusOk
+	var message = "Success"
+
+	var secureJSONData = req.PluginContext.DataSourceInstanceSettings.DecryptedSecureJSONData
+	if secureJSONData == nil {
+		secureJSONData = make(map[string]string)
+	}
+
+	// apiKey, apiKeyOk := secureJSONData["apiKey"]
+	_, apiKeyOk := secureJSONData["apiKey"]
+	if !apiKeyOk {
+		status = backend.HealthStatusError
+		message = "apiKey is required."
+	}
+
+	return &backend.CheckHealthResult{
+		Status:  status,
+		Message: message,
+	}, nil
+}
+
+type instanceSettings struct {
+	httpClient *http.Client
+}
+
+func newDataSourceInstance(setting backend.DataSourceInstanceSettings) (instancemgmt.Instance, error) {
+	return &instanceSettings{
+		httpClient: &http.Client{},
+	}, nil
+}
