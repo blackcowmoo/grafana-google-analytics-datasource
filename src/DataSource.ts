3faacb980ad82b819ea0508dcbf4895dde08341d
IM CHAECHEOL <dlacocjf32@gmail.com>
2020-10-10 01:52:28 +0900

    remove unused comment

--- src/DataSource.ts
+++ src/DataSource.ts
@@ -25,9 +25,6 @@ export class DataSource extends DataSourceWithBackend<GAQuery, GADataSourceOptio
   }
 
   async getProfileIds(accountId: string, webPropertyId: string): Promise<Array<SelectableValue<string>>> {
-    // let test = { aa: '123', bb: '456' };
-    // let abc = Object.entries(test).map(([value, label]) => ({ label, value } as SelectableValue<string>));
-    // return abc;
     return this.getResource('profiles', { accountId, webPropertyId }).then(({ profiles }) => {
       return profiles
         ? Object.entries(profiles).map(([value, label]) => ({ label, value } as SelectableValue<string>))
