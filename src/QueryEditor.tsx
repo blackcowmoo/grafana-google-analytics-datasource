d213b83bdb617d063c4a6b4f323d907db38c0d66
IM CHAECHEOL <dlacocjf32@gmail.com>
2020-10-10 01:54:20 +0900

    remove unused comment

--- src/QueryEditor.tsx
+++ src/QueryEditor.tsx
@@ -153,64 +153,6 @@ export class QueryEditor extends PureComponent<Props> {
             <div className="gf-form-label gf-form-label--grow" />
           </div>
         </div>
-        {/* <div className="gf-form-inline">
-          <InlineFormLabel
-            width={10}
-            className="query-keyword"
-            tooltip={
-              <p>
-                A string like <code>Sheet1!A1:B2</code>, that refers to a group of cells in the spreadsheet, and is
-                typically used in formulas.Named ranges are also supported. When a named range conflicts with a sheet's
-                name, the named range is preferred.
-              </p>
-            }
-          >
-            Range
-          </InlineFormLabel>
-          <input
-            className="gf-form-input width-14"
-            value={query.range || ''}
-            placeholder="ie: Class Data!A2:E"
-            onChange={this.onRangeChange}
-            onBlur={onRunQuery}
-          ></input>
-          <div className="gf-form gf-form--grow">
-            <div className="gf-form-label gf-form-label--grow" />
-          </div>
-        </div>
-        <div className="gf-form-inline">
-          <InlineFormLabel
-            width={10}
-            className="query-keyword"
-            tooltip="Time in seconds that the spreadsheet will be cached in Grafana after receiving a response from the spreadsheet API"
-          >
-            Cache Time
-          </InlineFormLabel>
-          <Segment
-            value={{ label: formatCacheTimeLabel(query.cacheDurationSeconds), value: query.cacheDurationSeconds }}
-            options={[0, 5, 10, 30, 60, 60 * 2, 60 * 5, 60 * 10, 60 * 30, 3600, 3600 * 2, 3600 * 5].map(value => ({
-              label: formatCacheTimeLabel(value),
-              value,
-              description: value ? '' : 'Response is not cached at all',
-            }))}
-            onChange={({ value }) => onChange({ ...query, cacheDurationSeconds: value! })}
-          />
-          <div className="gf-form gf-form--grow">
-            <div className="gf-form-label gf-form-label--grow" />
-          </div>
-        </div>
-        <div className="gf-form-inline">
-          <LegacyForms.Switch
-            label="Use Time Filter"
-            labelClass={'width-10  query-keyword'}
-            tooltip="Apply the dashboard time range to the first time field"
-            checked={query.useTimeFilter === true}
-            onChange={this.toggleUseTimeFilter}
-          />
-          <div className="gf-form gf-form--grow">
-            <div className="gf-form-label gf-form-label--grow" />
-          </div>
-        </div> */}
       </>
     );
   }
